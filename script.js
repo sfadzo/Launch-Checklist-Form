73bde6a8b131d057e5b1a76192e496fb3b8d98d4
Paul Rodriguez <paul_rodriguez@comcast.com>
2022-07-28 13:27:45 -0600

    launchstatus ready check

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Write your JavaScript code here!
 window.addEventListener("load",function(){
+
+   let itemStatusReveal = document.getElementById("itemStatus");
+   let launchStatusEval = document.getElementById("launchStatus");
+   //itemStatusReveal.style.visibility = "hidden";
    let theForm = document.getElementById("launchForm");
+
+   let pilotStatus = document.getElementById("pilotStatus");
+   let copilotStatus = document.getElementById("copilotStatus");
+   let fuelStatus = document.getElementById("fuelStatus");
+   let cargoStatus = document.getElementById("cargoStatus");
 
 
    theForm.addEventListener("submit", function(event){
@@ -24,8 +33,38 @@ window.addEventListener("load",function(){
 
        if (!pilotName || !copilotName || !fuelLevel || !cargoMass){
          alert("all fields must be completed");         
-       }else if (isNaN(pilotNameCheck) === false || isNaN(copilotNameCheck) === false || isNaN(fuelLevelCheck) === true || isNaN(cargoMassCheck) === true)
-         alert("Please enter valid information");
+       } else if (isNaN(pilotNameCheck) === false || isNaN(copilotNameCheck) === false || isNaN(fuelLevelCheck) === true || isNaN(cargoMassCheck) === true){
+         alert("Please enter valid information");         
+       }        
+         else{
+             itemStatusReveal.style.visibility = "visible";
+             pilotStatus.innerHTML = `Pilot Name: ${pilotName}`;
+             copilotStatus.innerHTML = `Copilot Name: ${copilotName}`;
+             fuelStatus.innerHTML = `Fuel Level: ${fuelLevel}`;
+             cargoStatus.innerHTML = `Cargo Mass: ${cargoMass}`;
+         }
+         if (fuelLevel < 10000 && cargoMass > 10000){
+            launchStatusEval.style.color= "red";
+            launchStatusEval.innerHTML = `Shuttle not ready for launch!`;
+            fuelStatus.innerHTML = `Fuel Level: ${fuelLevel} is to low for Launch!`;
+            cargoStatus.innerHTML = `Cargo Mass: ${cargoMass} is to heavy for Launch!`;
+         }else if (fuelLevel < 10000){
+            launchStatusEval.style.color= "red";
+            launchStatusEval.innerHTML = `Shuttle not ready for launch!`;
+            fuelStatus.innerHTML = `Fuel Level: ${fuelLevel} is to low for Launch!`;
+         }else if (cargoMass > 10000){
+            launchStatusEval.style.color= "red";
+            launchStatusEval.innerHTML = `Shuttle not ready for launch!`;
+            cargoStatus.innerHTML = `Cargo Mass: ${cargoMass} is to heavy for Launch!`;
+         }else {
+            launchStatusEval.style.color= "green";
+            launchStatusEval.innerHTML = `Shuttle is ready for launch!`;
+            fuelStatus.innerHTML = `Fuel Level: ${fuelLevel} check has passed!`;
+            cargoStatus.innerHTML = `Cargo Mass: ${cargoMass} check has passed!`;
+         }
+         
+           
+
    });
 
 
